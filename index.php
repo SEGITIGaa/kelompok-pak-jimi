633908f7e2fb58bec6a0eff1812683945eaf893b
ZhafranPratama <113425823+ZhafranPratama@users.noreply.github.com>
2022-10-25 10:51:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     </head>
     <body>
         <div class="form-container">
-            <form action="insert.php" method="post">
+            <form action="index.php" method="post">
                 <table >
                     <tr>
                         <td>
@@ -81,5 +81,21 @@
                 </form>
             </table>
         </div>
+<?php
+error_reporting(0);
+$kon = mysqli_connect('localhost', 'root', '', 'kelompok');
+
+$nama = $_POST['nama'];
+$usia = $_POST['usia'];
+$alamat = $_POST['alamat'];
+$agama = $_POST['agama'];
+$gender = $_POST['gender'];
+$pendidikan = $_POST['pendidikan'];
+$prestasi = $_POST['prestasi'];
+
+$sqlInput = "INSERT INTO data(nama,usia,alamat,agama,jenis_kelamin,pendidikan_akhir,prestasi) VALUES('$nama', '$usia', '$alamat', '$agama', '$gender', '$pendidikan', '$prestasi')";
+
+$insert = mysqli_query($kon, $sqlInput);
+?>
     </body>
 </html>
